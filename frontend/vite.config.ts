9c1bc35a6261a9e829405934f51709631307dc0f
John Howard Roberts <johnhowardroberts@users.noreply.github.com>
2025-05-09 18:09:04 +0100

    Update build configuration for static site generation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,5 +10,16 @@ declare module "@remix-run/node" {
 
 export default defineConfig({
   base: "/workout-generator/",
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
